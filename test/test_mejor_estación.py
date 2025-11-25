ebe7290dce9e38c05257023d5a89bae161f42030
goskus23 <141594539+goskus23@users.noreply.github.com>
2025-11-25 21:53:35 +0100

    test(test): :white_check_mark: Añadir tests para la funcion buscar_mejor_estacion

---
+++ test/test_mejor_estación.py
@@ -0,0 +1,22 @@
+from scr.buscar_mejor_estacion import buscar_mejor_estacion
+import pytest
+
+
+# Primero definimos los estados donde queremos ser escuchados
+estados_solicitados = set(["id","mo","ks","ar","co","sd","wy","nv","or"])
+
+
+# Estaciones de radio y estados que cubren
+
+
+@pytest.mark.mejor_estacion
+def test_mejor_estacion():
+    estaciones = {}
+    estaciones["kone"] = set(["id", "nv", "ut"])
+    estaciones["ktwo"] = set(["wa", "id", "mt"])
+    estaciones["kfive"] = set(["ca", "az"])
+
+
+    estados_cubiertos = ["wa","id","nv"]
+
+    assert buscar_mejor_estacion(estaciones,estados_cubiertos) == (2,"kfive")
